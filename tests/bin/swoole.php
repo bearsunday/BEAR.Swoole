906a8516d8754f7e6234687596cee35dd13bc105
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-11-13 18:13:17 +0900

    fix cs

--- tests/bin/swoole.php
+++ tests/bin/swoole.php
@@ -1,6 +1,8 @@
 <?php
+
+declare(strict_types=1);
 require dirname(__DIR__, 2) . '/vendor/autoload.php';
-exit((require dirname(__DIR__,2 ) . '/bootstrap.php')(
+exit((require dirname(__DIR__, 2) . '/bootstrap.php')(
     'prod-app',       // context
     'BEAR\Skeleton',      // application name
     '127.0.0.1',          // IP
