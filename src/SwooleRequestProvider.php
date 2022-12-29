5284d0a683a00b5dd96fe9fda3e37c42ced4b169
Akihito Koriyama <akihito.koriyama@gmail.com>
2022-12-30 04:01:20 +0900

    Update SwooleServerRequest

--- src/SwooleRequestProvider.php
+++ src/SwooleRequestProvider.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace BEAR\Swoole;
 
-use FastD\Http\SwooleServerRequest;
 use Psr\Http\Message\ServerRequestInterface;
 use Ray\HttpMessage\RequestProviderInterface;
 
